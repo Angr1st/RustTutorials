7d0b9453cbfaff181194052085e5677f7cee7320
Torben Clasen <torbenclasen@gmail.com>
2018-03-23 18:35:09 +0100

    Handling wrong input restarting the loop.

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
     println!("The secret number is: {}", secret_number);
 
     loop {
-        
+
         println!("Please input your guess.");
 
         let mut guess = String::new();
@@ -21,14 +21,20 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal =>{ 
+                println!("You win!");
+                break;
         }
+      }
     }
 }
