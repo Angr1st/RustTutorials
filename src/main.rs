484708865135965ee3187dd3857079530966a70e
Torben Clasen <torbenclasen@gmail.com>
2018-03-24 00:54:27 +0100

    shadowing; just reuse variable name with let

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
- let mut x = 5;
+ let x = 5;
+
+ let x = x + 10;
+ 
+ let x = x*x;
+ 
  println!("The value of x is: {}",x);
- x = 6;
- println!("The value of x is: {}",x);
- const MAX_POINTS: u32 = 100_000;
- println!("The value of MAX_POINTS is: {}",MAX_POINTS );
 }
