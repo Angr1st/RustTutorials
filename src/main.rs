f558b650ecf5ba2020ee476dd8d414af83b8167b
Torben Clasen <torbenclasen@gmail.com>
2018-03-24 01:29:50 +0100

    multiple parameters

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    another_function(4);
+    another_function(4,34234);
 }
 
-fn another_function(x: i32) {
+fn another_function(x: i32, y: i32) {
     println!("The value of x is:{}", x );
+    println!("The value of y is:{}", y );
 }
