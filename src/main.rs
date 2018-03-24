e608d58545e5286b6a6b13f269eb7714f1187232
Torben Clasen <torbenclasen@gmail.com>
2018-03-24 02:02:27 +0100

    for loop

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 fn main() {
     let numbers = [11,22,33,44,55,66,77];
-    let mut index = 0;
 
-    while index < 7 {
-        println!("{}!", numbers[index] );
-
-        index = index + 1;
+    for element in numbers.iter() {
+        println!("{}!", element );
     }
 }
