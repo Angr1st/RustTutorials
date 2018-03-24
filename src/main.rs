fd79b6d146aeb61c6572b8903e68162e832a6652
Torben Clasen <torbenclasen@gmail.com>
2018-03-24 01:25:55 +0100

    first new function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
-let array = [1,2,5,5,2,4];
-println!("The value of y is: {}", array[0]);
 
-let months = ["January", "February", "March", "April", "May", "June", "July",
-              "August", "September", "October", "November", "December"];
+println!("Hello, world!");
 
+another_function();
+}
 
-println!("The value of y is: {}", months[12]);
+fn another_function() {
+    println!("Another function!" );
 }
