34baf0b204c645337de508113d8bfe3f1b1e79c3
Torben Clasen <torbenclasen@gmail.com>
2018-03-24 02:01:08 +0100

    while over array

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 fn main() {
-    let mut number = 3;
-    
-    while number != 0 {
-        println!("{}!", number );
+    let numbers = [11,22,33,44,55,66,77];
+    let mut index = 0;
 
-        number = number - 1;
+    while index < 7 {
+        println!("{}!", numbers[index] );
+
+        index = index + 1;
     }
-
-    println!("LIFTOFF!!" );
 }
