df90b276152020cf19a82ec474234810aab266c8
Torben Clasen <torbenclasen@gmail.com>
2018-03-24 01:06:17 +0100

    Some bools

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,8 @@
 fn main() {
-let sum = 10 + 5 ;
+let t = true ;
 
-println!("{}",sum );
+let f:bool = false;
 
-let difference = 10.7 - 5.234 ;
-
-println!("{}",difference );
-
-let product = 4 * 43;
-
-println!("{}", product);
-
-let quotient = 56.7 / 45.6;
-
-println!("{}", quotient);
-
-let remainder = 87 % 5;
-
-println!("{}", remainder);
+let t = f && t;
+println!("{}",t );
 }
