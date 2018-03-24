101e9ed46cc4bd5076800a8bcbe71e679cd9fe5d
Torben Clasen <torbenclasen@gmail.com>
2018-03-24 01:04:44 +0100

    quick math

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 fn main() {
-let x = 2.0; // f64
+let sum = 10 + 5 ;
 
-let y: f32 = 3.0; // f32
+println!("{}",sum );
+
+let difference = 10.7 - 5.234 ;
+
+println!("{}",difference );
+
+let product = 4 * 43;
+
+println!("{}", product);
+
+let quotient = 56.7 / 45.6;
+
+println!("{}", quotient);
+
+let remainder = 87 % 5;
+
+println!("{}", remainder);
 }
