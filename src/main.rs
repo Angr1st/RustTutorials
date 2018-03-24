ffa4e4dead45cc8546e8bbc1d32d8cf8aebb8391
Torben Clasen <torbenclasen@gmail.com>
2018-03-24 01:45:07 +0100

    control flow if else

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {//Some super smart comment
-    println!("The value of y is: {}", 7);
+fn main() {
+    let number = 3;
+
+    if number > 5 {
+       println!("The value of y is: {}", number > 5); 
+    } else {
+     println!("The value of y is: {}", number > 5); 
+    } 
 }
