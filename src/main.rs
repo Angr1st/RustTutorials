4bde86dfa64b4b7af27306385a6ae705a22a92f2
Torben Clasen <torbenclasen@gmail.com>
2018-04-15 19:03:40 +0200

    13-11 using Cacher struct to cache the result of the closure

--- src/main.rs
+++ src/main.rs
@@ -41,25 +41,25 @@ impl<T> Cacher<T>
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_closure = |num: u32| -> u32 {
+    let mut expensive_result = Cacher::new(|num| {
     println!("Calculating slowly...");
     thread::sleep(Duration::from_secs(2));
     num
-    };
+    });
     if intensity < 25 {
         println!("Today, do {} pushups!",
-        expensive_closure(intensity)
+        expensive_result.value(intensity)
         );
         println!(
             "Next, do {} situps!",
-           expensive_closure(intensity)
+           expensive_result.value(intensity)
         );
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
             println!("Today, run for {} minutes!",
-            expensive_closure(intensity)
+            expensive_result.value(intensity)
             );
         }
     }
