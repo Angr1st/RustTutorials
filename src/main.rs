36e2096c529de8dea0b7d763f8a801d885bca61f
Torben Clasen <torbenclasen@gmail.com>
2018-04-15 17:07:22 +0200

    13-1 a func for hypothetical calc

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,12 @@
-use std::io;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
-    println!("Convert Fahrenheit to Celsius.");
-
-    println!("Please input your temperature.");
-
-    let mut temperature = String::new();
-
-    io::stdin().read_line(&mut temperature)
-        .expect("Failed to read line");
-
-    let temperature: f64 = temperature.trim().parse()
-        .expect("Please type a number!");
-
-    let temp_celsius = to_celsius(temperature);
-
-    println!("The temperature in Celsius is: {}", temp_celsius);
-
-    println!("The temperature in Fahrenheit is: {}", to_fahrenheit( temp_celsius));
+ simulated_expensive_calculation(10);
 }
 
-fn to_celsius(fahrenheit: f64) -> f64 {
-    (fahrenheit - 32.0) * (5.0/9.0)
-}
-
-fn to_fahrenheit(celsius: f64) -> f64 {
-    celsius * 1.8 + 32.0
+fn simulated_expensive_calculation(intensity: u32) -> u32 {
+    println!("Calculating slowly...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
 }
