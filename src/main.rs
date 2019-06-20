ad1143fa322cb2dee723d68b8afe1433a51482ae
Torben Clasen <torbenclasen@gmail.com>
2019-06-20 23:24:02 +0200

    Reset

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
 fn main() {
-    println!("1:{}; 2:{}; 50:{}; 300:{};", fibbonaci(1), fibbonaci(2), fibbonaci(50),fibbonaci(300))
-}
-
-const GOLDEN_RATIO:f64 = 1.618033988749894848204586834365638117720309179805762862135;
-const NEGATIV_GOLDEN_RATIO:f64 = -0.61803398874989484820458683436563811772030917980576286213;
-const FIVE:f64 = 5.0_f64;
-
-fn fibbonaci(n:i32) -> f64 {
-    let result = (GOLDEN_RATIO.powi(n) - NEGATIV_GOLDEN_RATIO.powi(n)) / (FIVE.sqrt());
-    result.round()
+    println!("reset yet again!");
 }
