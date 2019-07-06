6ecd030d0df14a571add03f67e5c98886d82e576
Torben Clasen <torbenclasen@gmail.com>
2019-07-07 00:44:22 +0200

    calculate the mean of a list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() { 
-        println!("Reset!")
+    let list:Vec<usize> = vec![12,812,1,0,22,6132,13];
+    let mean = sum(&list) / list.len();
+    println!("The mean is: {}",mean);
+}
+
+fn sum(list:&Vec<usize>) -> usize {
+    let mut result:usize = 0;
+    for number in list {
+        result = result + number;
+    }
+    result
 }
