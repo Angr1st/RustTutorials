7530c8ce8e39a2b16a04d86f0b30c2af07755577
Torben Clasen <torbenclasen@gmail.com>
2019-07-07 21:15:02 +0200

    piglatin test one

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 fn main() {
     let sentence = "Off to do the piglatin thing!";
-    let mut iter = sentence.split_whitespace();
+    let piglatin = transform_to_piglatin(sentence);
 
-    println!();
+    println!("Oy piglatin: {}",piglatin);
 
 }
 
 enum WordKind<'a> {
     Vocal(&'a str),
     Consonant(&'a str)
+}
+
+fn transform_to_piglatin(sentence:&str) -> String {
+    let iter = sentence.split_whitespace();
+    transform_words(sort_words(iter))
 }
 
 fn sort_words(words:std::str::SplitWhitespace) -> Vec<WordKind> {
@@ -24,19 +29,30 @@ fn sort_words(words:std::str::SplitWhitespace) -> Vec<WordKind> {
     vec
 }
 
+fn transform_words(words:Vec<WordKind>) -> String {
+    let mut result = String::new();
+    for word in words {
+        match word {
+            WordKind::Vocal(a) => result.push_str(&vocal(&a)),
+            WordKind::Consonant(a) => result.push_str(&consonants(&a))
+        }
+    }
+    result
+}
+
 fn is_vocal(char:char) -> bool {
     char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u' || char == 'A' || char == 'E' || char == 'I' || char == 'O' || char == 'U'
 }
 
 fn vocal(word:&str) -> String {
-    let lastpart = "-hay";
+    let lastpart = "-hay ";
     format!("{}{}",word,lastpart)
 }
 
 fn consonants(word:&str) -> String {
-    let firstchar = word.chars().nth(0).expect("A Word should at least contain one character!");
+    let firstchar = word.chars().nth(0).expect("A Word should at least contain one character!").to_lowercase();
     let rest = get_rest(word);
-    let lastpart = "ay";
+    let lastpart = "ay ";
     format!("{}-{}{}",rest,firstchar,lastpart)
 }
 
