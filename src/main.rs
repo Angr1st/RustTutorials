506a070dab69e5e0f325841ca8258e7e608784f4
Torben Clasen <torbenclasen@gmail.com>
2019-07-07 18:40:36 +0200

    Sort Words by starting with vocal or not

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
-fn main() { 
-    println!("Off to do the piglatin thing!");
+fn main() {
+    let sentence = "Off to do the piglatin thing!";
+    let mut iter = sentence.split_whitespace();
+
+    println!();
+
+}
+
+enum WordKind<'a> {
+    Vocal(&'a str),
+    Consonant(&'a str)
+}
+
+fn sort_words(words:std::str::SplitWhitespace) -> Vec<WordKind> {
+    let mut vec = Vec::new();
+    for word in words {
+        if is_vocal(word.chars().nth(0).expect("A word should contain at least one char!")) {
+            vec.push(WordKind::Vocal(word));
+        }
+        else {
+            vec.push(WordKind::Consonant(word));
+        }
+    }
+    vec
+}
+
+fn is_vocal(char:char) -> bool {
+    char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u' || char == 'A' || char == 'E' || char == 'I' || char == 'O' || char == 'U'
 }
 
 fn vocal(word:&str) -> String {
