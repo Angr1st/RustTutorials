bc08f059cbe9bbec3dde041b69dc6c23e375c1ab
Torben Clasen <torbenclasen@gmail.com>
2019-07-07 14:07:47 +0200

    created average fn

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 fn main() { 
     let list:Vec<usize> = vec![12,812,1,0,22,6132,13];
-    let mean = sum(&list) / list.len();
+    let mean = average(&list);
     println!("The mean is: {}",mean);
+}
+
+fn average(list:&Vec<usize>) -> usize {
+    sum(&list) / list.len()
 }
 
 fn sum(list:&Vec<usize>) -> usize {
